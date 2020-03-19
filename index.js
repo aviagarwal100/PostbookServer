a520931dc2e2aa605839d8cea60e97f2fbb73dbe
Avi <aviagarwal100@gmail.com>
2020-03-19 12:26:57 -0700

    stting

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect(db, { useNewUrlParser: true })
   .then(() => {
+    app.use(cors());
+
+    app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin","https://postbk.herokuapp.com"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+});
     console.log("mongobd connected successfully...");
   })
   .catch(err => {
@@ -34,17 +44,7 @@ app.use(bodyparser.json());
 app.get("/", (req, res) => {
   res.send("Hey bigStack");
 });
-app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin","https://postbk.herokuapp.com"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 // actual route ...
 app.use("/api/auth", auth);
 app.use("/api/photo", photo);
