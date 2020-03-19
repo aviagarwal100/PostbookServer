7be8ff654e3d48838ca22413109e7dae36076021
Avi <aviagarwal100@gmail.com>
2020-03-18 20:51:09 -0700

    setting

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 const auth = require("./routes/api/auth");
 const photo = require("./routes/api/photo");
+var cors = require('cors');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,18 +23,13 @@ app.use(passport.initialize());
 // passport-jwt configuration...
 require("./Strategies/passport")(passport);
 
-app.use((req, res, next) => { //doesn't send response just adjusts it
-    res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
-    );
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-        return res.status(200).json({});
-    }
-    next(); //so that other routes can take over
-})
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
