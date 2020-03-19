8cfe98676dcc112c5560af9b0847fd19003a897e
Avi <aviagarwal100@gmail.com>
2020-03-18 20:44:11 -0700

    setting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 const auth = require("./routes/api/auth");
 const photo = require("./routes/api/photo");
-//var cors = require('cors');
+var cors = require('cors');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -23,14 +23,16 @@ app.use(passport.initialize());
 // passport-jwt configuration...
 require("./Strategies/passport")(passport);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin","http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({ origin: true }));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 // route for testing ...
