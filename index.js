cc0b3b914853015aab1f0caa108141cf98e63c04
Avi <aviagarwal100@gmail.com>
2020-03-18 20:14:22 -0700

    setting

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 const auth = require("./routes/api/auth");
 const photo = require("./routes/api/photo");
+var cors = require('cors');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,15 +22,18 @@ mongoose
 app.use(passport.initialize());
 // passport-jwt configuration...
 require("./Strategies/passport")(passport);
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://postbook.netlify.com"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({
+  origin: 'https://postbook.netlify.com',
+  credentials: true,
+}));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 // route for testing ...
