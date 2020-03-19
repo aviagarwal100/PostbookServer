8c239c8a708c0f80fd6e881f57a0cbaee72a5890
Avi <aviagarwal100@gmail.com>
2020-03-18 20:18:45 -0700

    setting

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ mongoose
 app.use(passport.initialize());
 // passport-jwt configuration...
 require("./Strategies/passport")(passport);
-app.use(cors({
-  
-}));
+app.use(cors());
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
 //   res.header(
