17502ce4d0ab75dfee877d0748159013c25f08af
Avi <aviagarwal100@gmail.com>
2020-03-19 09:33:12 -0700

    next

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(function(req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  next();  
 });
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
