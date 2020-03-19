a17403fbf59adcd4565e7807c56f15b6a1cac830
Avi <aviagarwal100@gmail.com>
2020-03-19 11:29:15 -0700

    cors

--- index.js
+++ index.js
@@ -9,17 +9,7 @@ const auth = require("./routes/api/auth");
 const photo = require("./routes/api/photo");
 var cors = require('cors');
 
-app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin","https://postbk.herokuapp.com"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -28,6 +18,7 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
 
 // passport middleware
 app.use(passport.initialize());
@@ -42,6 +33,17 @@ app.use(bodyparser.json());
 // route for testing ...
 app.get("/", (req, res) => {
   res.send("Hey bigStack");
+});
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin","https://postbk.herokuapp.com"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 // actual route ...
 app.use("/api/auth", auth);
