23c1ae3c5826aaf7ebbd9cf8b896845c3f579577
Avi <aviagarwal100@gmail.com>
2020-03-18 18:33:50 -0700

    website address

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 require("./Strategies/passport")(passport);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://postbook.netlify.com"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
