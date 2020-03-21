da7edfb78d2002a3b8954b3d2faaf03f84fd42f2
Avi <aviagarwal100@gmail.com>
2020-03-21 01:33:31 -0700

    commit

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ app.get("/", (req, res) => {
   res.send("Hey bigStack");
 });
 app.use(cors());
-
+https://postbk.herokuapp.com/
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin","https://postbk.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin","https://postbk.herokuapp.com/"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header(
     "Access-Control-Allow-Headers",
